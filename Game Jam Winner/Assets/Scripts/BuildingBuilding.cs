382d77f5c92ab88491cedbfe3944f7e8a6dda360
Kind-Canadian <ninjacam_2007@icloud.com>
2024-08-19 15:59:28 -0600

    what what
    Fixed day night system. Just needs minor changes based on the other systems I don't have access to

--- Game Jam Winner/Assets/Scripts/BuildingBuilding.cs
+++ Game Jam Winner/Assets/Scripts/BuildingBuilding.cs
@@ -8,6 +8,7 @@ public class BuildingBuilding : MonoBehaviour
     public GameObject pblock;
     public GameObject tblock;
     public GameObject gblock;
+    public GameManager game;
     //The list effectively mimics the blocks order on screen just in a list instead
     //index 0 is the base of the building and the highest index is the top
     public List<GameObject> pBlocks = new List<GameObject>();
@@ -19,41 +20,52 @@ public class BuildingBuilding : MonoBehaviour
         pBlocks.Add(GameObject.FindGameObjectWithTag("pBase"));
         tBlocks.Add(GameObject.FindGameObjectWithTag("tBase"));
         gBlocks.Add(GameObject.FindGameObjectWithTag("gBase"));
+        game = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         if (Input.GetMouseButtonDown(0))
         {
             pbuild();
             tbuild();
             gbuild();
         }
-
-
-        if (Input.GetMouseButtonDown(1) && pBlocks.Count > 1)
+        */
+        //Debug.Log(Mathf.Floor(game.Pst_Population / 40));
+        if(Mathf.Ceil(game.Pst_Population/40) > pBlocks.Count)
         {
-            GameObject des = pBlocks[0];
-            pBlocks.RemoveAt(0);
-            Destroy(des);
+            pbuild();
         }
-        if (Input.GetMouseButtonDown(1) && tBlocks.Count > 1)
+
+        if (Mathf.Ceil(game.Thf_Population / 20) > tBlocks.Count)
         {
-            GameObject des = tBlocks[0];
-            tBlocks.RemoveAt(0);
-            Destroy(des);
+            tbuild();
         }
-        if (Input.GetMouseButtonDown(1) && gBlocks.Count > 1)
+
+        if (Mathf.Ceil(game.Grd_Population / 10) > gBlocks.Count)
         {
-            GameObject des = gBlocks[0];
-            gBlocks.RemoveAt(0);
-            Destroy(des);
+            gbuild();
+        }
+
+        if (Mathf.Ceil(game.Pst_Population/40) < pBlocks.Count && pBlocks.Count > 1)
+        {
+            pDestroy();
+        }
+        if (Mathf.Ceil(game.Thf_Population / 20) < tBlocks.Count && tBlocks.Count > 1)
+        {
+            tDestroy();
+        }
+        if (Mathf.Ceil(game.Grd_Population / 10) < gBlocks.Count && gBlocks.Count > 1)
+        {
+            gDestroy();
         }
 
         if (!pBlocks[0].CompareTag("Base"))
         {
-            Debug.Log("gay");
+            
             pBlocks[0].tag = "Base";
         }
         if(!pBlocks[pBlocks.Count - 1].CompareTag("Top") && pBlocks.Count > 1) 
@@ -97,5 +109,26 @@ public class BuildingBuilding : MonoBehaviour
     {
         GameObject addon = Instantiate(gblock, new Vector3(gBlocks[0].transform.position.x, gBlocks[gBlocks.Count - 1].transform.position.y + 10, gBlocks[0].transform.position.z), gBlocks[0].transform.rotation, null);
         gBlocks.Add(addon);
+    }
+
+    void pDestroy()
+    {
+        GameObject des = pBlocks[0];
+        pBlocks.RemoveAt(0);
+        Destroy(des);
+    }
+
+    void tDestroy()
+    {
+        GameObject des = tBlocks[0];
+        tBlocks.RemoveAt(0);
+        Destroy(des);
+    }
+
+    void gDestroy()
+    {
+        GameObject des = gBlocks[0];
+        gBlocks.RemoveAt(0);
+        Destroy(des);
     }
 }
